9a46beb9babafd022ab4837962b00d0fe77ee80d
codeamac19 <codeamac19@gmail.com>
2016-12-01 10:40:22 -0600

    Agrega la funcion before filters que se solicita

--- db/migrate/20161123172233_create_url.rb
+++ db/migrate/20161123172233_create_url.rb
@@ -1,6 +1,7 @@
 class CreateUrl < ActiveRecord::Migration
   def change
   create_table :urls do |t|
+    t.belongs_to :user, index: true 
     t.string :original
     t.string :short_url
     t.integer :click_count, default: 0
